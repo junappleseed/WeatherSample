a1718a6f37ea5d81bc82da4d4624f18919d48dfa
junappleseed <junappleseed@gmail.com>
2015-10-17 23:39:29 +0900

    地域選択用のテーブルを追加。

--- WeatherSample/ViewController.swift
+++ WeatherSample/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var areaTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        areaTableView.dataSource = self
+        areaTableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Area.items.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: .Default, reuseIdentifier: "Cell")
+        cell.textLabel!.text = Area.items[indexPath.row].name
+        return cell
+    }
 }
 
