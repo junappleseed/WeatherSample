de73f9f11e1462641f757eb9b9db7fea2a046264
junappleseed <junappleseed@gmail.com>
2015-10-22 01:32:02 +0900

    [add]天気情報取得機能の追加
    livedoorのお天気Webサービスを使用し、天気情報を取得する機能を追加しました。

--- WeatherSample/MainViewController.swift
+++ WeatherSample/MainViewController.swift
@@ -20,10 +20,49 @@ class MainViewController: UIViewController {
         
         self.navigationItem.title = selectedArea.areaName + "の天気"
         areaLabel.text = selectedArea.areaName
+        
+        showWeatherData(selectedArea.cityId)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    private func showWeatherData(cityId: String) {
+
+        let url = NSURL(string: "http://weather.livedoor.com/forecast/webservice/json/v1?city=" + cityId)
+        let request = NSURLRequest(URL: url!)
+        
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: config, delegate: nil, delegateQueue: NSOperationQueue.mainQueue())
+    
+        let task = session.dataTaskWithRequest(request, completionHandler: {
+            (data, response, error) in
+            
+            do {
+                let json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
+                
+                print(json["title"])
+                
+                if let location: NSDictionary = NSDictionary(dictionary: json["location"] as! NSDictionary) {
+                    print(location["area"])
+                    print(location["prefecture"])
+                    print(location["city"])
+                }
+                
+                if let forecasts: NSArray = NSArray(array: json["forecasts"] as! NSArray) {
+                    if let today: NSDictionary = NSDictionary(dictionary: forecasts[0] as! NSDictionary) {
+                        print(today["dateLabel"])
+                        print(today["telop"])
+                        print(today["date"])
+                    }
+                }
+                
+            } catch {
+                print("エラー")
+            }
+        })
+        task.resume()
     }
 }
